94a56ccd30f2e223601ff4776f0247b253def549
Christian <cwooddel@u.rochester.edu>
2021-01-21 16:47:36 -0500

    built on rspecs

--- towers/lib/towers.rb
+++ towers/lib/towers.rb
@@ -3,7 +3,7 @@ class Towers
   attr_reader :stacks
 
   def initialize
-
+    @stacks = [[4,3,2,1],[],[]]
   end
 
 end
--- towers/spec/towers_spec.rb
+++ towers/spec/towers_spec.rb
@@ -1,14 +1,37 @@
 require "rspec"
-require "lib"
+require "towers"
 
+describe Towers do 
+  subject(:game) {Towers.new}
 
-
-describe "#initialize" do
-  it "should create an array containing 3 sub arrays" do
-    expect(Towers.new).to ([[4,3,2,1],[],[]])
+  describe "#initialize" do
+    it "should create an array containing 3 sub arrays" do
+      expect(game.stacks).to eq([[4,3,2,1],[],[]])
+    end
   end
-end
 
-describe "#move" do
-  it ""
-end
+  describe "#move" do
+    it "should call #gets" do 
+
+    end 
+
+    it "moves a disk from one stack array to another" do 
+
+    end 
+
+    it "does not allow larger disks to be placed on smaller disks" do 
+
+    end 
+  end
+
+  describe "#won?" do 
+    it "returns true if second or third stack contains all disks" do 
+
+    end 
+
+    it "returns false if win? condition is not met" do 
+
+    end
+  end 
+
+end 
